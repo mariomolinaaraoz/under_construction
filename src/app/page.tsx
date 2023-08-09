838c74265b19c625b6bacfb16f5393e47ae830c9
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-08-09 14:29:43 -0300

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import Image from "next/image";
 import BackgroundImage from "./components/BackgroundImage";
 
@@ -9,8 +9,10 @@ export default function Home() {
       <BackgroundImage />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        For contact:&nbsp;
-          <code className="font-mono font-bold">mariomolina@steeldetailer.com.ar</code>
+          For contact:&nbsp;
+          <code className="font-mono font-bold">
+            mariomolina@steeldetailer.com.ar
+          </code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -33,14 +35,21 @@ export default function Home() {
       </div>
 
       <div className="mt-80 relative flex flex-col place-items-center before:absolute before:h-[0px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1 className="font-bold text-6xl relative dark:drop-shadow-[0_0_1.7rem_#ffffff90]">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          className="animate-pulse duration-1000 text-xl relative dark:drop-shadow-[0_0_1.7rem_#ffffff70]"
+          width={100}
+          height={24}
+          priority
+        />
+        <h1 className="font-bold text-5xl relative dark:drop-shadow-[0_0_1.7rem_#ffffff90]">
           STEEL DETAILING
         </h1>
-        <h2 className="animate-pulse duration-1000 text-xl relative dark:drop-shadow-[0_0_1.7rem_#ffffff70]">
+        <h2 className="text-xl relative dark:drop-shadow-[0_0_1.7rem_#ffffff70]">
           under construction
         </h2>
       </div>
-      
     </main>
   );
 }
