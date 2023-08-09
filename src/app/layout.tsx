b051633a6b6563cae72d4db1727d383c0e9edf77
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-08-09 14:19:55 -0300

    add metada, favicon, logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,14 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Steel Detailer',
-  description: 'Steel detailer site',
+  title: 'Mario Moina - Steel Detailer',
+  description: 'Mario Molina - Steel Detailer, provides steel detailing services for industry and steel fabricators. Among our services are Industrial steel detailing, Structural steel detailing, Miscellaneous steel detailing, Shop drawing production, BIM modeling and Fabrication support.',
+  // Agregar propiedades Open Graph
+  ogTitle: 'Mario Molina - Steel Detailer',
+  ogDescription: 'Providing top-quality steel detailing services for various industries.',
+  ogImage: '/path/to/og-image.jpg', // Ruta de la imagen de vista previa para compartir en redes sociales
+  ogUrl: 'https://www.steeldetailer.com.ar', // URL canónica de la página
+  
 }
 
 export default function RootLayout({
@@ -16,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      
     </html>
   )
 }
